2f6b1c955595418f1f60fda63ee7319cff0c46b0
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-31 15:09:50 -0400

    navegando atravez de history

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Prueba extends Component {
+    
+    handleClick = () => {
+        const { history } = this.props
+        //cuando usuario inicia sesion de forma exitosa en la aplicacion
+        //o cuando necesitamos cerrala sesion
+        history.push('/invoices')
+    }
+    
     render() {
-        return 'este es el componenete Pruebas!'
+        return <div onClick={ this.handleClick }>'este es el componenete Pruebas!'</div>
     }
 }
